394e4454df3309cd45f8b0517e848ce9143d0132
Septian Abiyoga <septianyoga111@gmail.com>
2023-05-11 11:51:44 +0700

    update env

--- app/Views/layout/v_footer_admin.php
+++ app/Views/layout/v_footer_admin.php
@@ -14,15 +14,15 @@
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Vendor JS Files -->
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/chart.js/chart.umd.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/echarts/echarts.min.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/quill/quill.min.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="<?= base_url('NiceAdmin') ?>/assets/vendor/php-email-form/validate.js"></script>
+<!-- Vendorr JS Files -->
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/apexcharts/apexcharts.min.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/chart.js/chart.umd.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/echarts/echarts.min.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/quill/quill.min.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/simple-datatables/simple-datatables.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/tinymce/tinymce.min.js"></script>
+<script src="<?= base_url('NiceAdmin') ?>/assets/vendorr/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
 <script src="<?= base_url('NiceAdmin') ?>/assets/js/main.js"></script>
