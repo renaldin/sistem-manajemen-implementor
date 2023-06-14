d6f5b6ffb37aa5e3fb016d94759081cedf969686
Septian Abiyoga <septianyoga111@gmail.com>
2023-06-15 06:31:49 +0700

    fixed error

--- app/Views/layout/v_nav_admin.php
+++ app/Views/layout/v_nav_admin.php
@@ -33,7 +33,7 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="<?= $user['foto'] == null ? base_url('foto_profile/default.png') : base_url('foto_profile/' . $user['foto']) ?>" alt="Profile" class="rounded-circle">
+                    <img src="<?= base_url('foto_profile/default.png') ?>" alt="Profile" class="rounded-circle">
                     <span class="d-none d-md-block dropdown-toggle ps-2"><?= session()->get('nama') ?></span>
                 </a>
                 <!-- End Profile Iamge Icon -->
